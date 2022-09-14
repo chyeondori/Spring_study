30c8a6bac13696e84c74b6f5331cd2cb5ad19b73
lch <lch.insideinfo@gmail.com>
2022-09-14 17:45:50 +0900

    #1 순수 Java로 service 구현 (AppConfig)

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -24,7 +24,7 @@ public class AppConfig {
     }
 
     public DiscountPolicy discountPolicy() {
-//        return new FixDiscountPolicy();
+//      return new FixDiscountPolicy();
         return  new RateDiscountPolicy();
     }
 }
