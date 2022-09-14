ceaf3c34176aad1adc6e9324f72a026052535e5e
lch <lch.insideinfo@gmail.com>
2022-09-14 18:04:00 +0900

    #2 Spring 컨테이너 사용해서 AppConfig 수정

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -9,23 +9,30 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
+    @Bean
     public MemberService memberService() { // 애플리케이션의 실제 동작에 필요한 구현 객체 생성
         return new MemberServiceImpl(memberRepository()); // 생성한 객체 인스턴스의 참조(레퍼런스)를 생성자를 통해 주입(연결)해줌
     }
 
-    private MemberRepository memberRepository() {
+    @Bean
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    @Bean
     public DiscountPolicy discountPolicy() {
 //      return new FixDiscountPolicy();
-        return  new RateDiscountPolicy();
+        return new RateDiscountPolicy();
     }
 }
 
