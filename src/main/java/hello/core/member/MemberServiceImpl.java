6bf1f0a4023db82a11ce157632b0cfc68091a1dd
chyeondori <chyeondori@gmail.com>
2022-09-22 17:06:40 +0900

    #5 / 컴포넌트 스캔, 옵션처리, 의존성 주입 학습 (test code 오류 있음)

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,9 +1,14 @@
 package hello.core.member;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class MemberServiceImpl implements MemberService {
 
     private final MemberRepository memberRepository;
 
+    @Autowired
     public MemberServiceImpl(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
@@ -17,7 +22,7 @@ public class MemberServiceImpl implements MemberService {
 
     //회원 조회
     @Override
-    public Member findMember(Long memberId){
+    public Member findMember(Long memberId) {
         return memberRepository.findbyId(memberId);
     }
 }
